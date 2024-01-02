3c5e465a396280135207ddf0be569c4629fddfa3
kavrad <kavyaht39@gmail.com>
2024-01-02 14:03:54 +0530

    Routes to perform all admin Roles

--- routes/adminRoles.js
+++ routes/adminRoles.js
@@ -1,16 +1,23 @@
+//import express
 const express=require("express");
-const { isLoggedIn, isUser, isAdmin } = require("../middleware/auth");
+
+const { isLoggedIn,  isAdmin } = require("../middleware/auth");
+
 const { getAllUsersDetails, updateUser, deleteUser, deleteAllUsers } = require("../controllers/adminRoles");
 
 
 const router=express.Router();
 
+//GET request to get all yser details
 router.get("/all-user-details",isLoggedIn,isAdmin,getAllUsersDetails);
 
+//PUT request to modify a user info
 router.put("/update-user",isLoggedIn,isAdmin,updateUser)
 
+//DELETE request to delete a particular user 
 router.delete("/delete-user/:userId",isLoggedIn,isAdmin,deleteUser);
 
+//DELETE request to delete all users 
 router.delete("/delete-all-users",isLoggedIn,isAdmin,deleteAllUsers);
 
 module.exports=router;
